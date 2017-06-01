c41304518006047be38ddc60f8cfaaa73e7fbe7b
Rahul Dongre <rahul.dongre@itgfirm.com>
2017-05-31 22:03:20 -0400

    imports updated

--- client/src/app/uber/trailer/trailer.module.ts
+++ client/src/app/uber/trailer/trailer.module.ts
@@ -1,32 +1,30 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import {TrailerService} from './trailer.service';
+import { TrailerService } from './trailer.service';
 
 
-import {TrailerRoutingModule} from './trailer-routing.module';
-import {TrailerShowComponent} from './trailer-show.component';
-import {TrailerListComponent} from './trailer-list.component';
-import {TrailerPersistComponent} from './trailer-persist.component';
-import { UserModule } from '../user/user.module';
-import { AddressModule } from '../address/address.module';
+import { TrailerRoutingModule } from './trailer-routing.module';
+import { TrailerShowComponent } from './trailer-show.component';
+import { TrailerListComponent } from './trailer-list.component';
+import { TrailerPersistComponent } from './trailer-persist.component';
+import { UserModule } from './../../user/user.module';
 
 @NgModule({
-  declarations: [
-    TrailerListComponent,
-    TrailerPersistComponent,
-    TrailerShowComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    TrailerRoutingModule,
-    UserModule,
-    AddressModule
-],
-  providers: [
-    TrailerService
-  ]
+	declarations: [
+		TrailerListComponent,
+		TrailerPersistComponent,
+		TrailerShowComponent
+	],
+	imports: [
+		CommonModule,
+		FormsModule,
+		TrailerRoutingModule,
+		UserModule
+	],
+	providers: [
+		TrailerService
+	]
 })
-export class TrailerModule {}
+export class TrailerModule { }
--- client/src/app/uber/truck/truck.module.ts
+++ client/src/app/uber/truck/truck.module.ts
@@ -1,32 +1,30 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import {TruckService} from './truck.service';
+import { TruckService } from './truck.service';
 
 
-import {TruckRoutingModule} from './truck-routing.module';
-import {TruckShowComponent} from './truck-show.component';
-import {TruckListComponent} from './truck-list.component';
-import {TruckPersistComponent} from './truck-persist.component';
-import { UserModule } from '../user/user.module';
-import { AddressModule } from '../address/address.module';
+import { TruckRoutingModule } from './truck-routing.module';
+import { TruckShowComponent } from './truck-show.component';
+import { TruckListComponent } from './truck-list.component';
+import { TruckPersistComponent } from './truck-persist.component';
+import { UserModule } from './../../user/user.module';
 
 @NgModule({
-  declarations: [
-    TruckListComponent,
-    TruckPersistComponent,
-    TruckShowComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    TruckRoutingModule,
-    UserModule,
-    AddressModule
-],
-  providers: [
-    TruckService
-  ]
+	declarations: [
+		TruckListComponent,
+		TruckPersistComponent,
+		TruckShowComponent
+	],
+	imports: [
+		CommonModule,
+		FormsModule,
+		TruckRoutingModule,
+		UserModule
+	],
+	providers: [
+		TruckService
+	]
 })
-export class TruckModule {}
+export class TruckModule { }
